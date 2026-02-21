0e4814019c81bc053f105e7582b910bffb3477b0
Vishakh Surendran <148840145+vishakhsurendran@users.noreply.github.com>
2026-02-20 20:02:31 -0500

    Delete backend/dist directory

--- backend/dist/routes/health.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const r = (0, express_1.Router)();
-r.get('/', (req, res) => res.json({ status: 'ok' }));
-exports.default = r;
