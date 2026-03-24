11b4a975301ba05108f49e0affe7d069b4dba662
jwpql <wu.julia573@gmail.com>
2026-03-24 02:30:52 -0400

    added popup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import search_icon from './search_icon.svg';
 
 function App() {
   const [text, setText]=useState("Search...");
+    const [isHidden, setIsHidden] = useState(false)
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -20,9 +21,24 @@ function App() {
     }
   }
 
+  function openPopup(){
+    setIsHidden(false);
+  }
+
+  function closePopup(){
+    setIsHidden(true)
+  }
+  
   return (
     <div className="App">
       <div>
+
+        {/* Temporary button to open popup. */}
+        <button onClick={openPopup} style={{position : 'fixed', zIndex : 10, marginLeft : 100}}>
+          Open popup
+        </button>
+
+        {/* Search bar and button. Text entry is stored in text variable */}
         <form id="search" onSubmit={handleSubmit}>
           <button id ="search_button">
               <img src={search_icon} style={{ width: "100%", height:"100%"}} alt="Search icon" />
@@ -31,9 +47,20 @@ function App() {
               <input id ="search_bar" type="text" value={text} onChange={handleChange} onClick={setEmpty}></input>
             </label>
         </form>
-        <div id="background"> 
-          put map here
+
+        {/* div where you would put the map? */}
+        <div id="background">
+          map here
         </div>
+
+        {/* empty pop up */}
+        <div id="pop-up" style={{ display: isHidden ? 'none' : 'block' }}>
+            <button onClick={closePopup} id="closePopupButton">
+              X
+            </button>
+            Popup contents
+        </div>
+
       </div>
     </div>
   );
